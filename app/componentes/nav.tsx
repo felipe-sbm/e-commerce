60b7f0ac97018e5541cd34dbacbf998281f85201
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-01-06 11:22:16 -0300

    Pequeno ajuste no site

--- app/componentes/nav.tsx
+++ app/componentes/nav.tsx
@@ -47,13 +47,6 @@ export const Navigation: React.FC = () => {
               Curriculum
             </Link>
             <Link
-              href="/lista"
-              className="duration-200 text-zinc-400 hover:text-zinc-100"
-            >
-              Lista de visitantes
-            </Link>
-
-            <Link
               href="/"
               className="duration-200 text-zinc-300 hover:text-zinc-100"
             >
