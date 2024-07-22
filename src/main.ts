f01e545c37c651ac04394fa822e97f066efb72cf
felipe-sbm <felipesbm2005@gmail.com>
2024-07-22 20:55:09 -0300

    Trocando para angular

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-createApp(App).use(router).mount('#app')
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
